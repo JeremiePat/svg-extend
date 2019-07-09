7f847825f6d19811adac3669c8c1a6bfaf172996
Jérémie Patonnier <jeremie.patonnier@gmail.com>
2019-07-09 18:42:26 +0200

    Add initial support for spirals

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import extendSVGElements from './lib/extendSVGElements.js'
 import ngon   from './lib/element.ngon.js'
 import star   from './lib/element.star.js'
+import spiral from './lib/element.spiral.js'
 
 // We preamptively initialized the library components
 extendSVGElements(ngon)
 extendSVGElements(star)
+extendSVGElements(spiral)
